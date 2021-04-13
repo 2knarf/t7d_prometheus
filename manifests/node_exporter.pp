f8399a35b6ffde559320ec5d9081a7aecd9762cc
Frank Solli <frank.solli@copyleft.no>
2021-04-13 16:22:03 +0200

    add systemctl

--- manifests/node_exporter.pp
+++ manifests/node_exporter.pp
@@ -1,3 +1,4 @@
+#Class for setting up node_exporter
 class t7d_prometheus::node_exporter {
 
 
@@ -5,13 +6,43 @@ class t7d_prometheus::node_exporter {
 
   file {'/usr/local/bin/node_exporter':
     ensure  => file,
-    mode    => 0755,
+    mode    => '0755',
     content =>('t7d_prometheus/node_exporter-1.1.2.linux-amd64/node_exporter')
   }
+
   user{'node_exporter':
     ensure     => present,
     comment    => 'Node exporter',
     shell      => '/usr/bin/false',
     managehome => false
   }
+  file {'/etc/systemd/system/node_exporter.service':
+    ensure  => present,
+    mode    => '0755',
+    content => ('t7d_prometheus/node_exporter.service')
+
+  }
+
+#sudo systemctl daemon-reload
+  exec {
+    'refresh_systemd':
+      command     => '/bin/systemctl daemon-reload',
+      subscribe   => File['/etc/systemd/system/node_exporter.service'],
+      refreshonly => true;
+  }
+#sudo systemctl start node_exporter
+  exec {
+    'start_node_exporter_systemd':
+      command     => '/bin/systemctl start node_exporter',
+      subscribe   => File['/etc/systemd/system/node_exporter.service'],
+      refreshonly => true;
+  }
+#sudo systemctl enable node_exporter
+  exec {
+    'enable_node_exporter_systemd':
+      command     => '/bin/systemctl enable node_exporter',
+      subscribe   => File['/etc/systemd/system/node_exporter.service'],
+      refreshonly => true;
+  }
+
 }
