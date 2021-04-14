8963322c72279719b4079fb929a306e2c06837f3
Frank Solli <frank.solli@copyleft.no>
2021-04-14 14:05:10 +0200

    update module with correct paths

--- manifests/node_exporter.pp
+++ manifests/node_exporter.pp
@@ -5,9 +5,9 @@ class t7d_prometheus::node_exporter {
   #TODO Make it possible to adjust version
 
   file {'/usr/local/bin/node_exporter':
-    ensure  => file,
-    mode    => '0755',
-    content =>('t7d_prometheus/node_exporter-1.1.2.linux-amd64/node_exporter')
+    ensure => file,
+    mode   => '0755',
+    source => 'puppet:///modules/t7d_prometheus/node_exporter-1.1.2.linux-amd64/node_exporter'
   }
 
   user{'node_exporter':
@@ -17,9 +17,9 @@ class t7d_prometheus::node_exporter {
     managehome => false
   }
   file {'/etc/systemd/system/node_exporter.service':
-    ensure  => present,
-    mode    => '0755',
-    content => ('t7d_prometheus/node_exporter.service')
+    ensure => present,
+    mode   => '0755',
+    source => 'puppet:///modules/t7d_prometheus/node_exporter.service'
 
   }
 
@@ -34,14 +34,14 @@ class t7d_prometheus::node_exporter {
   exec {
     'start_node_exporter_systemd':
       command     => '/bin/systemctl start node_exporter',
-      subscribe   => File['/etc/systemd/system/node_exporter.service'],
+      require     => File['/etc/systemd/system/node_exporter.service'],
       refreshonly => true;
   }
 #sudo systemctl enable node_exporter
   exec {
     'enable_node_exporter_systemd':
       command     => '/bin/systemctl enable node_exporter',
-      subscribe   => File['/etc/systemd/system/node_exporter.service'],
+      require     => File['/etc/systemd/system/node_exporter.service'],
       refreshonly => true;
   }
 
