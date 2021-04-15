995e8d69c9046de1f4e0157bcae1cc18f5c32aff
Frank Solli <frank.solli@copyleft.no>
2021-04-15 11:07:32 +0200

    add new server

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,4 +1,5 @@
 #Server setup for Prometheus
 class t7d_prometheus::server {
   #TODO collect resources from client and populate to config
+  Concat::Fragment <<| tag == 'node_exporter_service' |>>
 }
