403e27bfd45a5df07320d0e76b0790fdf535f9ff
Frank Solli <frank.solli@copyleft.no>
2021-04-15 13:33:47 +0200

    add file

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,5 +1,8 @@
 #Server setup for Prometheus
 class t7d_prometheus::server {
   #TODO collect resources from client and populate to config
+  concat { '/tmp/exporttest-franktest':
+  ensure => present,
+}
   Concat::Fragment <<| tag == 'node_exporter_service' |>>
 }
