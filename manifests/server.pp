3f84537bb91fb9bae07b91bb24dab1860b0bb949
Frank Solli <frank.solli@copyleft.no>
2021-10-21 16:54:02 +0200

    Puppet sucks. Notify will force restart and does not support reload

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,14 +2,9 @@
 #   Server setup for Prometheus
 class t7d_prometheus::server {
 
-  service { 'prometheus':
-    ensure => 'running'
-  }
-
   concat { '/etc/node-targets.yml':
     ensure_newline => true,
     owner          => 'root',
-    notify         => Service['prometheus'],
     group          => 'root',
     mode           => '0665',
   }
