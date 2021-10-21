61d22327c494c0c3601f67d887e4428ef2d49dd6
Frank Solli <frank.solli@copyleft.no>
2021-10-21 09:59:34 +0200

    collect resources

--- manifests/server.pp
+++ manifests/server.pp
@@ -8,15 +8,6 @@ class t7d_prometheus::server {
     mode           => '0660',
   }
 
-  # We need this to make sure it will become a valid YAML file, so this needs to be at the beginning of the file.
-  # This also where we could add custom labels
-  concat::fragment { 'node-targets-header':
-    target  => "${job_targets_file}",
-    content => "---\n- labels:\n  environment: production\n- targets:\n",
-    order   => 0,
-  }
-
-  # This is where the magic happens and how we collect the resources exported by the other nodes! \o/
-  T7d_Prometheus::Node_Exporter <<| |>>
+  Concat::Fragment <<| tag == 'node_exporter_service' |>>
 
 }
