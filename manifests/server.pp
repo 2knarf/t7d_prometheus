ee45d0749dfbf9db3465853049ae4069e2d34cd2
Frank Solli <frank.solli@copyleft.no>
2021-10-21 10:04:31 +0200

    correct ownership and mode

--- manifests/server.pp
+++ manifests/server.pp
@@ -3,9 +3,9 @@
 class t7d_prometheus::server {
   concat { '/tmp/node-targets.yaml':
     ensure_newline => true,
-    owner          => 'prometheus',
-    group          => 'prometheus',
-    mode           => '0660',
+    owner          => 'root',
+    group          => 'root',
+    mode           => '0665',
   }
 
   Concat::Fragment <<| tag == 'node_exporter_service' |>>
