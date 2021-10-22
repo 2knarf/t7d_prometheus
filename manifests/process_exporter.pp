e5ff97d3d9981447ca253a26be91df199384e0b0
Frank Solli <frank.solli@copyleft.no>
2021-10-22 09:57:53 +0200

    add process exporter

---
+++ manifests/process_exporter.pp
@@ -0,0 +1,68 @@
+# @summary
+#  Class for setting up process_exporter and exporting configuration to PuppetDB
+# @param version
+#   Which version of process_exporter should be used
+# @example
+#   include t7d_prometheus::process_exporter
+# @example
+#   File: data/nodes/puppetdevclient03.teknograd.no.yaml
+#   ---
+#   classes:
+#    - t7d_prometheus::process_exporter
+# @see
+#   https://github.com/ncabatoff/process-exporter 
+class t7d_prometheus::process_exporter (String $version = '0.7.7') {
+
+  file {'/usr/local/bin/process_exporter':
+    ensure => file,
+    mode   => '0755',
+    source => "puppet:///modules/t7d_prometheus/process-exporter-${version}.linux-amd64/proces-exporter"
+  }
+
+  user{'process_exporter':
+    ensure     => present,
+    comment    => 'process exporter',
+    shell      => '/bin/false',
+    managehome => false
+  }
+
+  file {'/etc/systemd/system/process_exporter.service':
+    ensure => present,
+    mode   => '0755',
+    source => 'puppet:///modules/t7d_prometheus/process_exporter.service'
+
+  }
+  file {'/etc/default/process_exporter':
+    ensure  => present,
+    mode    => '0755',
+    require => File['/etc/systemd/system/process_exporter_service'],
+    source  => 'puppet:///modules/t7d_prometheus/default/process_exporter'
+
+  }
+
+  # sudo systemctl daemon-reload is required to pick up changes in the systemd directory
+  exec {
+    'refresh_systemd':
+      command     => '/bin/systemctl daemon-reload',
+      subscribe   => File['/etc/systemd/system/process_exporter.service'],
+      require     => File['/etc/systemd/system/process_exporter.service'],
+      refreshonly => true;
+  }
+
+  # Start and enable node_exporter with builtin service type from Puppet
+  service{ 'process_exporter':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/usr/local/bin/process_exporter'],
+    require   => [Exec['refresh_systemd'],File['/usr/local/bin/process_exporter']],
+    hasstatus => true,
+  }
+
+  #  This should end up like this on the prom server:  - targets: ['apt-web01.teknograd.no:9113']
+  @@concat::fragment { "process_exporter_service-${::fqdn}":
+    target  => '/etc/process-targets.yml',
+    content => "- targets: ['${::fqdn}:9256']",
+    tag     => 'process_exporter_service',
+  }
+
+}
--- manifests/server.pp
+++ manifests/server.pp
@@ -15,6 +15,14 @@ class t7d_prometheus::server {
     group          => 'root',
     mode           => '0665',
   }
+  concat { '/etc/process-targets.yml':
+    ensure_newline => true,
+    owner          => 'root',
+    notify         => Exec['refresh_prometheus'],
+    group          => 'root',
+    mode           => '0665',
+  }
 
   Concat::Fragment <<| tag == 'node_exporter_service' |>>
+  Concat::Fragment <<| tag == 'process_exporter_service' |>>
 }
