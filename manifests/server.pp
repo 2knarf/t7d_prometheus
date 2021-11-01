9ae53609c050933bca6618abe99f4e9019d17c2e
Frank Solli <frank.solli@copyleft.no>
2021-11-01 15:44:28 +0100

    disable collecting on the server side

--- manifests/server.pp
+++ manifests/server.pp
@@ -22,15 +22,15 @@ class t7d_prometheus::server {
     group          => 'root',
     mode           => '0665',
   }
-  concat { '/etc/systemd-targets.yml':
-    ensure_newline => true,
-    owner          => 'root',
-    notify         => Exec['refresh_prometheus'],
-    group          => 'root',
-    mode           => '0665',
-  }
+  #  concat { '/etc/systemd-targets.yml':
+  #  ensure_newline => true,
+  #  owner          => 'root',
+  #  notify         => Exec['refresh_prometheus'],
+  #  group          => 'root',
+  #  mode           => '0665',
+  #}
 
   Concat::Fragment <<| tag == 'node_exporter_service' |>>
   Concat::Fragment <<| tag == 'process_exporter_service' |>>
-  Concat::Fragment <<| tag == 'systemd_exporter_service' |>>
+  #  Concat::Fragment <<| tag == 'systemd_exporter_service' |>>
 }
