701b7c46a117cbf872af483d2b162587a188d276
Frank Solli <frank.solli@copyleft.no>
2021-11-08 15:10:48 +0100

    add collecting of varnish to server

--- manifests/server.pp
+++ manifests/server.pp
@@ -41,5 +41,6 @@ class t7d_prometheus::server {
   Concat::Fragment <<| tag == 'process_exporter_service' |>>
   Concat::Fragment <<| tag == 'apache_exporter_service' |>>
   Concat::Fragment <<| tag == 'nginx_exporter_service' |>>
+  Concat::Fragment <<| tag == 'varnish_exporter_service' |>>
   #  Concat::Fragment <<| tag == 'systemd_exporter_service' |>>
 }
