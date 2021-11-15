f38f7184f1a9295bc5511178aca51df88dd20cfc
Frank Solli <frank.solli@copyleft.no>
2021-11-15 12:38:06 +0100

    collect nginx

--- manifests/server.pp
+++ manifests/server.pp
@@ -43,6 +43,13 @@ class t7d_prometheus::server {
     group          => 'root',
     mode           => '0665',
   }
+  concat { '/etc/nginx-targets.yml':
+    ensure_newline => true,
+    owner          => 'root',
+    notify         => Exec['refresh_prometheus'],
+    group          => 'root',
+    mode           => '0665',
+  }
   #  concat { '/etc/systemd-targets.yml':
   #  ensure_newline => true,
   #  owner          => 'root',
