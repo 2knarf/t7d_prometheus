2f2e9b199444402492d6b8a4db692e24f0f37a0d
Frank Solli <frank.solli@copyleft.no>
2021-11-23 14:23:31 +0100

    update status path

--- manifests/nginx_exporter.pp
+++ manifests/nginx_exporter.pp
@@ -15,7 +15,7 @@
 #
 class t7d_prometheus::nginx_exporter
 (
-  String $scrape_uri = '"http://localhost/stub_status"',
+  String $scrape_uri = '"http://localhost/server-status"',
   String $version = '0.9.0'
 )
 
