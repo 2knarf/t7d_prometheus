d2b57a9517f16cd8df9c06e52345c5e3baa8cf87
Frank Solli <frank.solli@copyleft.no>
2021-12-08 14:33:36 +0100

    update

--- manifests/textfile/setup.pp
+++ manifests/textfile/setup.pp
@@ -1,5 +1,13 @@
 #Setup the base for textfile exporter
 class t7d_prometheus::textfile::setup {
+  case $facts['os']['name'] {
+    'RedHat','CentOS': {
+      include t7d_prometheus::textfile::yum
+    }
+    /^(Debian|Ubuntu)$/:  {
+      include t7d_prometheus::textfile::apt
+    }
+  }
 
     file {'/var/lib/prometheus/node-exporter':
       ensure  => directory,
