79d7b37136d1c4ef4de5f05913d3e469631d703b
Frank Solli <frank.solli@copyleft.no>
2021-12-08 14:44:31 +0100

    update

--- manifests/textfile/apt.pp
+++ manifests/textfile/apt.pp
@@ -20,16 +20,16 @@ class t7d_prometheus::textfile::apt {
   exec {
     'refresh_systemd_apt_timer':
       command     => '/bin/systemctl daemon-reload',
-      subscribe   => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','prometheus-node-exporter-apt.service'],
-      require     => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','prometheus-node-exporter-apt.service'],
+      subscribe   => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','/etc/systemd/system/prometheus-node-exporter-apt.service'],
+      require     => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','/etc/systemd/system/prometheus-node-exporter-apt.service'],
       refreshonly => true;
   }
 
   service{ 'prometheus-node-exporter-apt.timer':
     ensure    => running,
     enable    => true,
-    subscribe => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','prometheus-node-exporter-apt.service'],
-    require   => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','prometheus-node-exporter-apt.service'],
+    subscribe => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','/etc/systemd/system/prometheus-node-exporter-apt.service'],
+    require   => File['/etc/systemd/system/prometheus-node-exporter-apt.timer','/etc/systemd/system/prometheus-node-exporter-apt.service'],
     hasstatus => true,
   }
 }
